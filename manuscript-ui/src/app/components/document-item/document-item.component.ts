10103f45f60c14719e6e47e70da927db9543e1a5
eladinbar <62742789+eladinbar@users.noreply.github.com>
2023-04-16 13:19:05 +0300

    Added the document-item component - used to pinpoint specific annotations.

---
+++ manuscript-ui/src/app/components/document-item/document-item.component.ts
@@ -0,0 +1,23 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AnnotationModel} from "../../models/AnnotationModel";
+
+@Component({
+  selector: 'app-document-item',
+  templateUrl: './document-item.component.html',
+  styleUrls: ['./document-item.component.css']
+})
+export class DocumentItemComponent implements OnInit {
+  @Input() annotation!: AnnotationModel;
+  @Output() onSelectAnnotation: EventEmitter<AnnotationModel> = new EventEmitter();
+  @Output() onUpdateAnnotation: EventEmitter<AnnotationModel> = new EventEmitter();
+  @Output() onDeleteAnnotation: EventEmitter<AnnotationModel> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(annotation: AnnotationModel) {
+    this.onSelectAnnotation.emit(annotation);
+  }
+}
