f5665945ed7090175f683871a67b656c2ffc959b
Oz Madmoni <ozmdm@post.bgu.ac.il>
2023-05-24 17:20:02 +0300

    new updates added: document-info-dialog component to view image info.

---
+++ manuscript-ui/src/app/components/dialogs/document-info-dialog/document-info-dialog.component.ts
@@ -0,0 +1,16 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-document-info-dialog',
+  templateUrl: './document-info-dialog.component.html',
+  styleUrls: ['./document-info-dialog.component.css']
+})
+export class DocumentInfoDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DocumentInfoDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  // Add any additional methods or logic as needed
+}
