54966177b2c2a4e67cb819cdb942076005b6bf2d
eladinbar <62742789+eladinbar@users.noreply.github.com>
2023-06-10 03:31:23 +0300

    Added text wrap functionality for annotations, text no longer overflows outside of the specified box if it can be fully contained within.

--- manuscript-ui/src/app/components/pages/document-details/document-details.component.ts
+++ manuscript-ui/src/app/components/pages/document-details/document-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, OnInit, ViewChild, Inject} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {DocumentService} from "../../../services/document.service";
 import {ActivatedRoute} from "@angular/router";
 import {DomSanitizer} from "@angular/platform-browser";
@@ -20,9 +20,9 @@ import {Observable} from "rxjs";
 export class DocumentDetailsComponent implements OnInit {
   NIL: string = "00000000-0000-0000-0000-000000000000";
 
-  @Input() uid = '';
-  @Input() documentId = '';
-  @Input() title = '';
+  @Input() uid: string = '';
+  @Input() documentId: string = '';
+  @Input() title: string = '';
 
   coordinates: AnnotationCoordinatesModel[] = [];
   annotations: AnnotationModel[] = [];
@@ -282,13 +282,71 @@ export class DocumentDetailsComponent implements OnInit {
         // Add text
         const text: string = value;
         const textX: number = startX + width / 2;
-        const textY: number = startY + height / 2 + 3.5;
+        // Modify according to font size (calibrated to 14px)
+        let textY: number = startY + height / 2 + 3.5;
         this.ctx.font = 'bold 14px Arial';
         this.ctx.fillStyle = 'red';
         this.ctx.textAlign = 'center';
-        this.ctx.fillText(text, textX, textY);
+
+        // Modify according to font size (calibrated to 14px)
+        let wrappedText = this.wrapText(this.ctx, text, textX, textY, Math.abs(width), 20);
+        // If text has multiple lines, start filling text from top of box rather than center and recalculate wrapped text
+        if(wrappedText.length > 1) {
+          // Modify according to font size (calibrated to 14px)
+          textY = Math.min(startY, endY) + 14;
+        }
+        // Modify according to font size (calibrated to 14px)
+        wrappedText = this.wrapText(this.ctx, text, textX, textY, Math.abs(width), 20);
+        wrappedText.forEach((item) => {
+          // @ts-ignore
+          this.ctx!.fillText(item[0], item[1], item[2]);
+        })
       }
     }
+  }
+
+// @description: wrapText wraps HTML canvas text onto a canvas of fixed width
+// @param ctx - the context for the canvas we want to wrap text on
+// @param text - the text we want to wrap.
+// @param x - the X starting point of the text on the canvas.
+// @param y - the Y starting point of the text on the canvas.
+// @param maxWidth - the width at which we want line breaks to begin - i.e. the maximum width of the canvas.
+// @param lineHeight - the height of each line, so we can space them below each other.
+// @returns an array of [ lineText, x, y ] for all lines
+  wrapText(ctx: CanvasRenderingContext2D, text: string, x: number, y: number, maxWidth: number, lineHeight: number) {
+    // First, start by splitting all of our text into words, but splitting it into an array split by spaces
+    let words: string[] = text.split(' ');
+    let line: string = ''; // This will store the text of the current line
+    let testLine: string = ''; // This will store the text when we add a word, to test if it's too long
+    let lineArray = []; // This is an array of lines, which the function will return
+
+    // Let's iterate over each word
+    for(let n = 0; n < words.length; n++) {
+      // Create a test line, and measure it..
+      testLine += `${words[n]} `;
+      let metrics: TextMetrics = ctx.measureText(testLine);
+      let testWidth: number = metrics.width;
+      // If the width of this test line is more than the max width
+      if (testWidth > maxWidth && n > 0) {
+        // Then the line is finished, push the current line into "lineArray"
+        lineArray.push([line, x, y]);
+        // Increase the line height, so a new line is started
+        y += lineHeight;
+        // Update line and test line to use this word as the first word on the next line
+        line = `${words[n]} `;
+        testLine = `${words[n]} `;
+      }
+      else {
+        // If the test line is still less than the max width, then add the word to the current line
+        line += `${words[n]} `;
+      }
+      // If we never reach the full max width, then there is only one line.. so push it into the lineArray so we return something
+      if(n === words.length - 1) {
+        lineArray.push([line, x, y]);
+      }
+    }
+    // Return the line array
+    return lineArray;
   }
 
   clearCanvas(): void {
